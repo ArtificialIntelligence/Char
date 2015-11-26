37711cf653bc10d084e4d68cef4a6e3b9d5ce800
Creator <wassil.janssen@gmail.com>
2015-11-26 20:56:24 +0200

    Fixed the faulty O.

--- Test.lua
+++ Test.lua
@@ -2,8 +2,13 @@
 	The test program to see if it works.
 ]]--
 
-os.loadAPI("CC/AI")
+os.loadAPI("CC/AI.lua")
+local AI = _G["AI.lua"]
 
 local myNet = AI.Net({15,32,32,32,8})
 
-myNet.trainFromDir("Char/Data",10)
+myNet.trainFromDir("Char/Data",10,true)
+
+local file = fs.open("Char/CharNN.lua","w")
+file.write(textutils.serialize(myNet.serialize()))
+file.close()
